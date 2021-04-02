b0aef4f2213209549225c6ba51ac210dafd1f64f
wancheng <caiwancheng@aliyun.com>
2021-04-02 17:10:07 +0800

    init commit

---
+++ app/src/main/java/com/ex/view/KChatMyApplication.java
@@ -0,0 +1,29 @@
+package com.ex.view;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.ex.view.util.DataUtils;
+
+
+/**
+ * author wancheng
+ * date   2020/4/29
+ * desc  The code can't block the young lady!
+ * version  v1.0
+ */
+public class KChatMyApplication extends Application {
+    public static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        DataUtils.destroy();
+    }
+}
